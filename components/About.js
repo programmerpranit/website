98a8f6f4aa0a2efe3578f61f37c801dbf69a469a
stockraja <105791801+stockraja@users.noreply.github.com>
2022-07-09 20:34:22 +0530

    init

---
+++ components/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section>
+      <div className="pcontainer pt-20 items-center flex md:flex-row flex-col-reverse">
+        <div className="image w-full md:w-1/2 md:visible invisible">
+            <img src="home-right.png" alt="" />
+        </div>
+
+        <div className="text md:w-1/2">
+
+          <h2>LET&rsquo;S INTRODUCE ABOUT MYSELF</h2>
+
+          <p className="mb-5 md:text-base text-sm">Whose given. Were gathered. There first subdue greater. Bearing you Whales heaven midst their. Beast creepeth. Fish days.</p>
+
+          <p className="mb-5 md:text-base text-sm">Is give may shall likeness made yielding spirit a itself together created after sea is in beast beginning signs open god you&rsquo;re gathering whose gathered cattle let. Creature whales fruit unto meat the life beginning all in under give two.</p>
+          
+          <button className="inline-flex text-white py-2 px-8 focus:outline-none rounded-md  hover:bg-white hover:text-blue border-2 mt-5">My Resume</button>
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ components/Navbar.js
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import Link from 'next/link'
+
+// function NavLink({to, children}) {
+//     return <a href={to} className={`mx-4`}>
+//         {children}
+//     </a>
+// }
+
+function MobileNav({open, setOpen}) {
+    return (
+        <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter  `}>
+            <div className="flex items-center justify-center filter  bg-white h-20"> {/*logo container*/}
+                <a className="text-xl font-semibold" href="#">Programmer Pranit</a>
+            </div>
+            <div className=" text-center flex flex-col ml-4 pt-5">
+
+                    <Link  href={'/about'}><p className='py-5' onClick={() => setTimeout(() => {setOpen(!open)}, 100)}> About </p></Link>
+                    <Link  href={'/about1'}><p className='py-5' onClick={() => setTimeout(() => {setOpen(!open)}, 100)}> Services </p></Link>
+                    <Link  href={'/about3'}><p className='py-5' onClick={() => setTimeout(() => {setOpen(!open)}, 100)}> Portfolio </p></Link>
+                    <Link  href={'/about4'}><p className='py-5' onClick={() => setTimeout(() => {setOpen(!open)}, 100)}> Blog </p></Link>
+                    <Link  href={'/about5'}><p className='py-5' onClick={() => setTimeout(() => {setOpen(!open)}, 100)}> Contact </p></Link>
+
+            </div>  
+        </div>
+    )
+}
+
+export default function Navbar() {
+
+    const [open, setOpen] = useState(false)
+    return (
+        <nav className="flex filter bg-white px-4 py-4 h-20 items-center pcontainer">
+            <MobileNav open={open} setOpen={setOpen}/>
+            <div className="w-3/12 flex items-center">
+                <a className="text-2xl font-semibold" href="#">Pranit</a>
+            </div>
+            <div className="w-9/12 flex justify-end items-center">
+
+                <div className="z-50 flex relative w-6 h-5 flex-col justify-between items-center md:hidden" onClick={() => {
+                    setOpen(!open)
+                }}>
+                    {/* hamburger button */}
+                    <span className={`h-0.5 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-2" : ""}`} />
+                    <span className={`h-0.5 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+                    <span className={`h-0.5 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-2.5" : ""}`} />
+                </div>
+
+                <div className="hidden md:flex list-none">
+
+                    <Link href={'/about1'}><li className='mx-5 text-sm text-black uppercase font-semibold'> About</li></Link>
+                    <Link href={'/about2'}><li className='mx-5 text-sm text-black uppercase font-semibold'> About</li></Link>
+                    <Link href={'/about3'}><li className='mx-5 text-sm text-black uppercase font-semibold'> About</li></Link>
+                    <Link href={'/about4'}><li className='mx-5 text-sm text-black uppercase font-semibold'> About</li></Link>
+
+
+                </div>
+            </div>
+        </nav>
+    )
+}
